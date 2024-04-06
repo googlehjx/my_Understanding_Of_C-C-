9c7f6d2cb23f6136a152457ed760b183f6826f57
jxhe <jxhe@mail.ustc.edu.cn>
2024-04-07 03:38:52 +0800

    面向对象

--- funciton/main.cpp
+++ funciton/main.cpp
@@ -1,9 +1,26 @@
+#include <iostream>
+#include <string>
 int add(int, int);
+
+class Student {
+private:
+	std::string name;
+	float score;
+public:
+	Student(std::string n, float s) :name(n), score(s) {}
+	void show() {
+		std::cout << "name: " << name << " " << "score: " << score << std::endl;
+	}
+
+
+};
 int main(int argc, char* argv[]) {
 	int x;
 	int y;
 	x = 1;
 	y = 2;
+	Student s("John", 99.0);
+	s.show();
 	return add(x, y);
 }
 
