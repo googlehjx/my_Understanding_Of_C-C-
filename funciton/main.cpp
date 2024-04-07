37602fc632aede3668c27f13794ba6ebb8bc6b66
He Jinxin <jxhe@mail.ustc.edu.cn>
2024-04-07 15:45:59 +0800

    添加lambda表达式

--- funciton/main.cpp
+++ funciton/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <string>
-int add(int, int);
-class Add_math {
+int add(int, int);   // 声明普通函数
+class Add_math {     // 定义类型，重写函数操作符，其对象可作为函数符使用
 public:
 	int opperator()(int a, int b){
 		return a+b;
 }
-
+auto fun = [](Student &s){std::cout<<"name: "<<s.name<<" Score: " <<s.score<<std::endl;
 class Student {
 private:
 	std::string name;
@@ -16,8 +16,6 @@ public:
 	void show() {
 		std::cout << "name: " << name << " " << "score: " << score << std::endl;
 	}
-
-
 };
 int main(int argc, char* argv[]) {
 	int x;
@@ -26,8 +24,8 @@ int main(int argc, char* argv[]) {
 	y = 2;
 	Student s("John", 99.0);
 	s.show();
-	Add_math add;
-	int sum = add(x, y);
+	Add_math add_functor;  
+	int sum = add_functor(x, y);
 	return add(x, y);
 }
 
