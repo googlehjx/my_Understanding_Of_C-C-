6e836ca744eacda2d255137dabaeded7d005af42
jxhe <jxhe@mail.ustc.edu.cn>
2024-04-07 19:11:39 +0800

    函数：模板函数、lambda表达式、函数符、普通函数、对象成员函数

--- funciton/main.cpp
+++ funciton/main.cpp
@@ -1,37 +1,48 @@
 #include <iostream>
 #include <string>
 int add(int, int);   // 声明普通函数
-class Add_math {     // 定义类型，重写函数操作符，其对象可作为函数符使用
-public:
-	int opperator()(int a, int b){
-		return a+b;
+
+template<typename T>  // 函数模板
+T add_temp(T a, T b) {
+	return a + b;
 }
 
-class Student {
-private:
-	std::string name;
-	float score;
+template<>   // 函数模板特化
+double add_temp<double>(double a, double b) {
+	std::cout << "specialization template function" << std::endl;
+	return a + 1.0;
+}
+
+class Add_math {     // 定义类型，重写函数操作符，其对象可作为函数符使用
 public:
-	Student(std::string n, float s) :name(n), score(s) {}
-	void show() {
-		std::cout << "name: " << name << " " << "score: " << score << std::endl;
+	int operator()(int a, int b) {
+		return a + b;
 	}
 };
 
-auto fun = [](Student &s){std::cout<<"name: "<<s.name<<" Score: " <<s.score<<std::endl;
+class Student {
+public:
+	std::string name;
+	float score;
+};
+
+// lambda表达式
+auto fun = [](Student& s) {std::cout << "name: " << s.name << " Score: " << s.score << std::endl; };
 int main(int argc, char* argv[]) {
 	int x;
 	int y;
 	x = 1;
 	y = 2;
-	Student s("John", 99.0);
-	s.show();
+	Student s{ "John", 99.0 };
 	Add_math add_functor;  
 	int sum = add_functor(x, y);
-	fun(s);
+	fun(s);    // lambda函数调用
+	add_temp<int>(x, y); // 函数模板显示实例化
+	add_temp<double>(1.0, 3.0);
 	return add(x, y);
 }
 
+// 函数定义
 int add(int a, int b) {
 	return a + b;
 }
